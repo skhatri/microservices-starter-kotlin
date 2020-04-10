d3b5d5a31a5c85bfa982448821681f83c6964b2b
Suresh Khatri <servlet2@msn.com>
2020-04-10 23:02:21 +1000

    add cors header for todo endpoint

--- app/src/main/kotlin/com/github/starter/app/todo/endpoints/TodoEndpoints.kt
+++ app/src/main/kotlin/com/github/starter/app/todo/endpoints/TodoEndpoints.kt
@@ -4,17 +4,12 @@ import com.github.starter.app.todo.model.TodoTask;
 import com.github.starter.app.todo.service.TodoService;
 import com.github.starter.core.container.Container;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/todo")
+@CrossOrigin(origins = ["*"])
 class TodoEndpoints(private val todoService: TodoService) {
 
     @GetMapping("/search")
